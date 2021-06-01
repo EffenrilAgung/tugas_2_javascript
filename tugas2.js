87eda57efb268d6330d822b7773790dc594f83ba
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:42:43 +0700

    Initial commit

---
+++ tugas2.js
@@ -0,0 +1,25 @@
+//number
+var number = 20;
+//string
+var karakter = "hello world";
+//null
+// eslint-disable-next-line no-undef
+hasil = null;
+//undifined
+var prediksi;
+// boolean
+tinggi = "lulus"
+var input = 165;
+if (input > 165)
+    tinggi = "tidak lulus";
+else
+    tinggi = "lulus";
+//array
+let angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// tampilkan
+console.log(number);
+console.log(karakter);
+console.log(hasil);
+console.log(tinggi);
+console.log(angka[5], angka);
+
